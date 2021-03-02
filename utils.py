48e575197240a285b12291ba93b67756f87a2cad
ethsu1 <eth.fmt@gmail.com>
2021-03-02 14:58:48 -0800

    back to pytorch tutorial

--- utils.py
+++ utils.py
@@ -25,34 +25,3 @@ def imshow(tensor, title=None):
   if title is not None:
     plt.title(title)
   plt.pause(0.001)
-
-
-def plot_stats(title, y_title, x_title, y_data, x_data=[], legend=[], path="./figures/",):
-  '''
-  plot graph based on arguments
-  Args:
-      title - title of graph
-      y_title - y-axis title
-      x_titile - x-axis title
-      y_data - data along y axis
-      x_data - data along x axis
-      legend - list of strings corresponding to each set of y_data 
-          (ie if you want to plot multiple sets of data on one graph)
-  '''
-  if(os.path.isdir(path) == False):
-    os.mkdir(path)
-  plt.figure()
-  length = y_data.shape[1]
-  if(len(x_data) == 0):
-    x = [i for i in range(1, length+1)]
-  else:
-    x = x_data
-  for i, y in enumerate(y_data):
-    plt.plot(x, y, label=legend[i])
-  plt.ylabel(y_title)
-  plt.xlabel(x_title)
-  plt.title(title)
-  plt.legend()
-  title = title.replace(" ", "_")
-  plt.savefig(path+title+".png")
-  
