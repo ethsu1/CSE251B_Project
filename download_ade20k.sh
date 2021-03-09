32218e7f7ef495b842bf51edfbe1f4ac1cfd27f9
Matheus Gorski <mgorski@eng.ucsd.edu>
2021-03-08 21:40:39 -0800

    some changes to ade20k downloader

--- download_ade20k.sh
+++ download_ade20k.sh
@@ -2,4 +2,6 @@ mkdir -p real_data;
 wget -O ./real_data/ADEChallengeData2016.zip http://data.csail.mit.edu/places/ADEchallenge/ADEChallengeData2016.zip;
 unzip ./real_data/ADEChallengeData2016.zip -d ./real_data;
 rm ./real_data/ADEChallengeData2016.zip;
+mv ./real_data/ADEChallengeData2016/images/training/* real_data;
+rm -r ./real_data/ADEChallengeData2016;
 echo "Dataset downloaded."
